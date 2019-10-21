bf91f622ae2cb78d404c514b8082c3e41283d686
victorbr2 <47553336+victorbr2@users.noreply.github.com>
2019-10-21 16:55:14 +0200

    map changes

--- dungeon-platformer-tileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.4" name="dungeon-platformer-tileset" tilewidth="16" tileheight="16" tilecount="60" columns="6">
- <image source="Game/maps/dungeon-platformer-tileset.png" width="96" height="160"/>
-</tileset>
