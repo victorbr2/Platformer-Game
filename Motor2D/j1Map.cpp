09ba221d6c72e85bebe158dc405dc612ee2e17c5
victorbr2 <47553336+victorbr2@users.noreply.github.com>
2019-10-25 09:57:05 +0200

    map resolution(not solved)

--- Motor2D/j1Map.cpp
+++ Motor2D/j1Map.cpp
@@ -47,7 +47,7 @@ void j1Map::Draw()
 						SDL_Rect r = tileset->GetTileRect(tile_id);
 						iPoint pos = MapToWorld(x, y);
 
-						App->render->Blit(tileset->texture, pos.x, pos.y, &r);
+						App->render->Blit(tileset->texture, pos.x, pos.y, &r,App->render->drawsize);
 					}
 				}
 			}
--- Motor2D/j1Render.cpp
+++ Motor2D/j1Render.cpp
@@ -123,14 +123,14 @@ void j1Render::ResetViewPort()
 }
 
 // Blit to screen
-bool j1Render::Blit(SDL_Texture* texture, int x, int y, const SDL_Rect* section, float speed, double angle, int pivot_x, int pivot_y) const
+bool j1Render::Blit(SDL_Texture* texture, int x, int y, const SDL_Rect* section, float speed, float size ,double angle, int pivot_x, int pivot_y) const
 {
 	bool ret = true;
 	uint scale = App->win->GetScale();
 
 	SDL_Rect rect;
-	rect.x = (int)(camera.x * speed) + x * scale;
-	rect.y = (int)(camera.y * speed) + y * scale;
+	rect.x = (int)(camera.x * speed) + x * scale*size;
+	rect.y = (int)(camera.y * speed) + y * scale*size;
 
 	if(section != NULL)
 	{
